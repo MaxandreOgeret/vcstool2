c502f7724df464fe7e1ba2071b0ef2678411b60c
Maxandre Ogeret <MaxandreOgeret@users.noreply.github.com>
2023-02-19 22:49:45 +0200

    Removed client init useless check

--- vcstool/clients/__init__.py
+++ vcstool/clients/__init__.py
@@ -1,9 +1,4 @@
 from .git import GitClient
 
 vcstool_clients = [GitClient]
-
 _client_types = [c.type for c in vcstool_clients]
-if len(_client_types) != len(set(_client_types)):
-    raise RuntimeError(
-        'Multiple vcs clients share the same type: ' +
-        ', '.join(sorted(_client_types)))
